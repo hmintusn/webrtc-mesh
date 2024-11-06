e452c9ba8d0c4cf8a02bf891fd210da91d5ee5e3
hgmerc27 <ndhg246@gmail.com>
2024-11-06 13:39:06 +0700

    Remote videos changes

--- src/main/resources/static/webrtc.js
+++ src/main/resources/static/webrtc.js
@@ -11,10 +11,10 @@ document.getElementById('peerId').textContent = `Your Peer ID: ${peerId}`;
 // WebSocket signaling server
 const signalingServer = new WebSocket("ws://localhost:8080/ws");
 
-signalingServer.onmessage = function(message) {
+signalingServer.onmessage = function (message) {
     const data = JSON.parse(message.data);
 
-    switch(data.type) {
+    switch (data.type) {
         case "user-joined":
             // When a new peer joins, create an offer for that peer
             if (data.peerId !== peerId) {
@@ -52,6 +52,9 @@ async function startStream() {
     try {
         localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
         localVideo.srcObject = localStream;
+        Object.values(peerConnections).forEach(pc => {
+            localStream.getTracks().forEach(track => pc.addTrack(track, localStream));
+        });
         // Notify server that the peer is ready
         signalingServer.send(JSON.stringify({
             type: "join",
@@ -186,6 +189,6 @@ function createPeerConnection(id) {
 }
 
 // Handle window close event
-window.onbeforeunload = function() {
+window.onbeforeunload = function () {
     stopStream();
 };
